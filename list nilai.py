5909fbdc02e1d3219fb3d0baaf20612c7b771a2c
rohani2101 <130126901+rohani2101@users.noreply.github.com>
2023-04-19 16:53:26 +0800

    Add files via upload

---
+++ list nilai.py
@@ -0,0 +1,30 @@
+#list nilai
+list_nilai = [1,2,3,4,5,6,7,8,9,10]
+print(list_nilai)
+#menggunkan eppend
+list_nilai.append(11)
+print(list_nilai)
+#menggunakan insert
+list_nilai.insert(0,"2")
+print(list_nilai)
+#menggunakan len
+list_nilai = [1,2,3,4,5,6,7,8,9,10]
+jumlah_index = len (list_nilai)
+print("=================================================================")
+
+list_nilai.insert(4,"97")
+print (list_nilai)
+print("==================================")
+list_nilai= [1,2,3,4,5,6,7,8,9,10]
+last_three = list_nilai[-3:]
+print (last_three)
+print("===============================")
+list_nilai=[1,2,3,4,5,6,7,8,9,10]
+list_nilai.remove(1)
+list_nilai.remove(2)
+print (list_nilai)
+print("================================")
+list_nilai= [1,2,3,4,5,6,7,8,9,10]
+last_seven = list_nilai [-7:]
+print (last_seven)
+print("===============================")
